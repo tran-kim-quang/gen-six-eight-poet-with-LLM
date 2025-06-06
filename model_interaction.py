9509a711e80042926d0bd6b0f7bbec1bc7a970e9
tran-kim-quang <107093325+tran-kim-quang@users.noreply.github.com>
2025-06-07 01:27:10 +0700

    Update model_interaction.py

--- model_interaction.py
+++ model_interaction.py
@@ -3,10 +3,12 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 model = GPT2LMHeadModel.from_pretrained('meomeo163/luc-bat-poet-model')
 tokenizer = GPT2Tokenizer.from_pretrained('meomeo163/luc-bat-poet-model')
 
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=device)
+
 
 prompt_text = "Ngẫm hay trăm sự tại trời"
 print(f"\nPrompt: '{prompt_text}'")
@@ -14,8 +16,14 @@ print(f"\nPrompt: '{prompt_text}'")
 try:
     generated_output = pipe(
         prompt_text,
-        max_length=100,
-        num_return_sequences=1,
+        max_length=100, # max length of poet
+        num_return_sequences=1, # max poet generated
+        do_sample=True,
+        temperature=0.7, # creative param
+        top_k=30,      # get highest proba of next 30 token
+        top_p=0.85,    # highest proba of token
+        no_repeat_ngram_size=3,
+        repetition_penalty=1.15,
         pad_token_id=tokenizer.pad_token_id,
         eos_token_id=tokenizer.eos_token_id,
     )
